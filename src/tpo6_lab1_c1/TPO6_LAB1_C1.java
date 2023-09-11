55072d7ac497391c7d91eeb0de1298f73492f560
GMTUDS <142052001+GMTUDS@users.noreply.github.com>
2023-09-10 23:01:08 -0300

    Initial commit

---
+++ src/tpo6_lab1_c1/TPO6_LAB1_C1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpo6_lab1_c1;
+
+/**
+ *
+ * @author nn
+ */
+public class TPO6_LAB1_C1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
